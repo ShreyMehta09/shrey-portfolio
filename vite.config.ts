7d5dea55580d76b59ea7bb7f0eda5d309b1c65e5
shreymehta09 <shreymehta2023@gmail.com>
2025-04-07 01:31:09 +0530

    downgrade-version

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,34 +13,10 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|webp|svg)$/i,
-      jpg: {
-        quality: 80,
-      },
-      png: {
-        quality: 80,
-      },
-      webp: {
-        lossless: true
-      },
-      cache: true
-    }),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          ui: ['@/components/ui'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  }
 }));
